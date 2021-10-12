bd8d988850623fd3d465070278f296d0a8b7b8e4
HonestlyDex <82985536+HonestlyDex@users.noreply.github.com>
2021-10-12 07:13:28 -0400

    Updated duedscript.js

--- duedscript.js
+++ duedscript.js
@@ -1,5 +1,5 @@
 //Credits: github.com/ThatFruedDued/blooket-hack
 
-//USE ON THE PLAY SCREEN OF BLOOKET THEN ADD YOUR SCRIPTS AND PRESS INJECT HACK
+//USE ON THE PLAY SCREEN OF BLOOKET THEN ADD YOUR SCRIPTS AND PRESS LAUNCH HACK
 
 fetch('https://thatfrueddued.github.io/blooket-hack/index.js').then(r=>r.text()).then(t=>(new Function(t))())
