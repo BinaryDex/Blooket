59f5f9c1a5fc1628c48c7293108b535619f86131
HonestlyDex <82985536+HonestlyDex@users.noreply.github.com>
2021-10-30 16:58:32 -0400

    Update and rename duedscript.js to main.js

--- main.js
+++ main.js
@@ -2,4 +2,4 @@
 
 //USE ON THE PLAY SCREEN OF BLOOKET THEN ADD YOUR SCRIPTS AND PRESS LAUNCH HACK
 
-fetch('https://thatfrueddued.github.io/blooket-hack/index.js').then(r=>r.text()).then(t=>(new Function(t))())
+fetch('https://thatfrueddued.github.io/blooket-utility/index.js').then(r=>r.text()).then(t=>(new Function(t))())
